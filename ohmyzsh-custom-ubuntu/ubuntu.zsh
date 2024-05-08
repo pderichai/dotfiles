7eb6fd2d990d6035f15379d2544d567ecce22597
Deric Pang <pderichai@gmail.com>
2024-05-08 13:49:24 -0400

    Updating ubuntu nvm.

--- ohmyzsh-custom-ubuntu/ubuntu.zsh
+++ ohmyzsh-custom-ubuntu/ubuntu.zsh
@@ -11,6 +11,10 @@ export LSCOLORS=ExExExExExEgedabagacad
 zstyle ':completion:*' list-colors ${(s.:.)LSCOLORS}
 
 export PATH=$HOME/bin:$PATH
+export PATH=/usr/local/cuda-12.4/bin:$PATH
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # History settings.
 export HISTFILE=~/.bash_eternal_history
