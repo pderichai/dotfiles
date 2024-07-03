e5680dcf1cf26f88a3ea92b456212b1bb194bac6
Deric Pang <dpang@figma.com>
2024-07-03 10:33:49 -0400

    Updating dotfiles and also python LSP in figma.

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -18,5 +18,28 @@ return {
         end,
 			},
 		},
+    setup = {
+      pyright = function()
+        local function set_python_path(path)
+				  local LspconfigUtil = require("lspconfig.util")
+          local clients = LspconfigUtil.get_lsp_clients {
+            bufnr = vim.api.nvim_get_current_buf(),
+            name = 'pyright',
+          }
+          for _, client in ipairs(clients) do
+            if client.settings then
+              client.settings.python = vim.tbl_deep_extend('force', client.settings.python, { pythonPath = path })
+            else
+              client.config.settings = vim.tbl_deep_extend('force', client.config.settings, { python = { pythonPath = path } })
+            end
+            client.notify('workspace/didChangeConfiguration', { settings = nil })
+          end
+        end
+
+        if vim.fn.getcwd() == "/Users/dpang/figma/figma/ml" then
+          set_python_path("~/figma/figma/ml/py/.dev_venv/bin/python")
+        end
+      end,
+    }
 	},
 }
